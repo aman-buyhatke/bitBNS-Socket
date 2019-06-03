3295c4bd562c4353beaee74e830b72fb14bebd2f
aman-buyhatke <51257795+aman-buyhatke@users.noreply.github.com>
2019-06-03 17:30:56 +0530

    added new events!

--- how2use.js
+++ how2use.js
@@ -1,10 +1,14 @@
-const socket = require('./socketBNS').coin('BTC');
+const socket = require('./socketBNS').coin('BTC')
+
+socket.on('connect', () => console.log('Connected'))
 
 socket.on('news', res => {
 	try {
-		const data = JSON.parse(res);
-		console.log(data);
+		const data = JSON.parse(res)
+		console.log('Data Received ', data)
 	} catch (e) {
-		console.log(e);
+		console.log('Error in new Stream ', e)
 	}
-});
+})
+
+socket.on('disconnect', () => console.log('Disconnected'))
