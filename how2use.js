b95dd4fa7adc9d8859d4ade0a95775cd13e9b1b3
aman-buyhatke <51257795+aman-buyhatke@users.noreply.github.com>
2019-06-03 17:52:28 +0530

    Update how2use.js

--- how2use.js
+++ how2use.js
@@ -1,13 +1,14 @@
 const socket = require('./socketBNS').coin('BTC')
-
+console.log();
 socket.on('connect', () => console.log('Connected'))
 
 socket.on('news', res => {
 	try {
 		const data = JSON.parse(res)
-		console.log('Data Received ', data)
+		console.log('Data Received')
+		console.log(data)
 	} catch (e) {
-		console.log('Error in new Stream ', e)
+		console.log('Error in the Stream', e)
 	}
 })
 
